206747c4f664567d80c8e005560b196ed4b142f0
giordano200699 <giordano200699@hotmail.com>
2019-04-11 02:19:35 -0500

    Obtencion de imagenes de manera facil

--- src/archivos/archivos.controller.ts
+++ src/archivos/archivos.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get, Post, Put, Delete, Body,Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body,Param,Res } from '@nestjs/common';
 import { ArchivosService } from "./archivos.service";
 import { Archivo } from "src/interfaces/Archivo";
+var fs = require('fs');
 
 @Controller('archivos')
 export class ArchivosController {
 	
 	constructor(private archivosService: ArchivosService){}
 	
+	@Get('imagen/:id/:numero')
+	obtenerImagenArchivo(@Param('id') id, @Param('numero') numero, @Res() respuesta) {
+		var objeto = this.archivosService.obtenerArchivo(id);
+		objeto.then(function(res){
+			var buf = Buffer.from(res[0].archivo.toString(), 'base64');  
+			fs.writeFile('fotos/foto'+numero+'.png', buf,function(){
+				return respuesta.sendFile('E:/Archivos/P_RecomendacionTemas/Backend/biblioteca-backend/fotos/foto'+numero+'.png');
+			});
+			
+		});
+	}
 	@Get(':id')
-	obtenerArchivo(@Param('id') id): Promise<Archivo> {
+	obtenerArchivo(@Param('id') id) : Promise<Archivo>{
 		return this.archivosService.obtenerArchivo(id);
 	}
 
