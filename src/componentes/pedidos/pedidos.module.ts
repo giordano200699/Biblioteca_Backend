63951d916c6ce371e75db03f6fc7c2071bb97bc2
giordano200699 <giordano200699@gmail.com>
2019-06-03 03:33:30 -0500

    crear pedido y actualizar item

--- src/componentes/pedidos/pedidos.module.ts
+++ src/componentes/pedidos/pedidos.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PedidosController } from "./pedidos.controller";
 import { PedidosService } from "./pedidos.service";
 import { PedidoSchema } from './../../esquemas/pedido.schema';
+import { ItemSchema } from './../../esquemas/item.schema';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Pedido', schema: PedidoSchema }])],
+	imports: [MongooseModule.forFeature([{ name: 'Pedido', schema: PedidoSchema },
+	{ name: 'Item', schema: ItemSchema }])],
 	controllers: [PedidosController],
 	providers: [PedidosService]
 })
--- src/componentes/pedidos/pedidos.service.ts
+++ src/componentes/pedidos/pedidos.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { Pedido } from "src/interfaces/Pedido";
+import { Item } from "src/interfaces/Item";
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class PedidosService {
 
-	constructor(@InjectModel('Pedido') private pedidoModelo: Model<Pedido>) {}
+	constructor(@InjectModel('Pedido') private pedidoModelo: Model<Pedido>,
+    @InjectModel('Item') private itemModelo: Model<Item>) {}
 
     async obtenerPedidos(){
 		return await this.pedidoModelo.find();
     }
     
     async crearPedido(pedido: Pedido){
+        await this.itemModelo.update({'itemId':pedido.itemId},{'disponibilidad':2});
         const ultimoPedido:Pedido = await this.pedidoModelo.findOne().sort({ pedidoId: 'desc'}).limit(1);
         if(ultimoPedido){
             pedido.pedidoId = ultimoPedido.pedidoId + 1;
