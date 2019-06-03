c7afef091e1f776052aadb682147e639223f543e
giordano200699 <giordano200699@gmail.com>
2019-06-03 04:22:15 -0500

    editando campo estado de alumno al crear el pedido

--- src/componentes/pedidos/pedidos.module.ts
+++ src/componentes/pedidos/pedidos.module.ts
@@ -4,10 +4,12 @@ import { PedidosController } from "./pedidos.controller";
 import { PedidosService } from "./pedidos.service";
 import { PedidoSchema } from './../../esquemas/pedido.schema';
 import { ItemSchema } from './../../esquemas/item.schema';
+import { UsuarioSchema } from './../../esquemas/usuario.schema';
 
 @Module({
 	imports: [MongooseModule.forFeature([{ name: 'Pedido', schema: PedidoSchema },
-	{ name: 'Item', schema: ItemSchema }])],
+	{ name: 'Item', schema: ItemSchema },
+	{ name: 'Usuario', schema: UsuarioSchema }])],
 	controllers: [PedidosController],
 	providers: [PedidosService]
 })
--- src/componentes/pedidos/pedidos.service.ts
+++ src/componentes/pedidos/pedidos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Pedido } from "src/interfaces/Pedido";
 import { Item } from "src/interfaces/Item";
+import { Usuario } from "src/interfaces/Usuario";
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -8,7 +9,8 @@ import { Model } from 'mongoose';
 export class PedidosService {
 
 	constructor(@InjectModel('Pedido') private pedidoModelo: Model<Pedido>,
-    @InjectModel('Item') private itemModelo: Model<Item>) {}
+    @InjectModel('Item') private itemModelo: Model<Item>,
+    @InjectModel('Usuario') private usuarioModelo: Model<Usuario>) {}
 
     async obtenerPedidos(){
 		return await this.pedidoModelo.find();
@@ -16,6 +18,7 @@ export class PedidosService {
     
     async crearPedido(pedido: Pedido){
         await this.itemModelo.update({'itemId':pedido.itemId},{'disponibilidad':2});
+        await this.usuarioModelo.update({'dni':pedido.usuarioId},{'estado':1});
         const ultimoPedido:Pedido = await this.pedidoModelo.findOne().sort({ pedidoId: 'desc'}).limit(1);
         if(ultimoPedido){
             pedido.pedidoId = ultimoPedido.pedidoId + 1;
