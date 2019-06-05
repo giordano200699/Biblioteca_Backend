bd8c9b6fa09819f959b268b428b456ac398cb7fe
giordano200699 <giordano200699@gmail.com>
2019-06-05 11:53:13 -0500

    Servicio de autentificacion mejorado con pedidos

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -4,9 +4,11 @@ import { UsuariosController } from "./usuarios.controller";
 import { UsuariosService } from "./usuarios.service";
 import { UsuarioSchema } from './../esquemas/usuario.schema';
 import { CuentaSchema } from './../esquemas/cuenta.schema';
+import { PedidoSchema } from './../esquemas/pedido.schema';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Usuario', schema: UsuarioSchema },{ name: 'Cuenta', schema: CuentaSchema }])],
+	imports: [MongooseModule.forFeature([{ name: 'Usuario', schema: UsuarioSchema },{ name: 'Cuenta', schema: CuentaSchema },
+	{ name: 'Pedido', schema: PedidoSchema }])],
 	controllers: [UsuariosController],
 	providers: [UsuariosService]
 })
--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Usuario } from "src/interfaces/Usuario";
 import { Cuenta } from "src/interfaces/Cuenta";
+import { Pedido } from "src/interfaces/Pedido";
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class UsuariosService {
 
-	constructor(@InjectModel('Usuario') private usuarioModelo: Model<Usuario>,@InjectModel('Cuenta') private cuentaModelo: Model<Cuenta>) {}
+	constructor(@InjectModel('Usuario') private usuarioModelo: Model<Usuario>,@InjectModel('Cuenta') private cuentaModelo: Model<Cuenta>,
+	@InjectModel('Pedido') private pedidoModelo: Model<Pedido>) {}
 
 	async obtenerUsuarios(){
 		return await this.usuarioModelo.find();
@@ -48,17 +50,41 @@ export class UsuariosService {
 	}
 
 	async esUsuario(cuenta){
-		var objeton = this;
-		return this.cuentaModelo.find({"nombre":cuenta.nombre,"contrasenia":cuenta.contrasenia}).then(async function(res){
-			if(res.length==1){
-				return await objeton.usuarioModelo.find({'dni':res[0].idUsuario});
+		var resultado = [];
+		const cuentas = await this.cuentaModelo.find({"nombre":cuenta.nombre,"contrasenia":cuenta.contrasenia});
+		if(cuentas.length==1){
+			const usuario = await this.usuarioModelo.findOne({'dni':cuentas[0].idUsuario});
+			const pedidosRechazados = await this.pedidoModelo.find({'estado':0,'usuarioId':usuario.dni}).count();
+			const pedidosActivos = await this.pedidoModelo.find({'estado':1,'usuarioId':usuario.dni}).count();
+			const pedidosAceptados = await this.pedidoModelo.find({'estado':2,'usuarioId':usuario.dni}).count();
+			await resultado.push({
+				"_id": usuario._id,
+	            "nombres": usuario.nombres,
+	            "apellidos": usuario.apellidos,
+	            "edad": usuario.edad,
+	            "dni": usuario.dni,
+	            "imagenId": usuario.imagenId,
+	            "sexo": usuario.sexo,
+	            "estado": usuario.estado,
+	            "codigo": usuario.codigo,
+	            "correoInstitucional": usuario.correoInstitucional,
+	            "correoPersonal": usuario.correoPersonal,
+	            "escuelaId": usuario.escuelaId,
+	            "telefonoCasa": usuario.telefonoCasa,
+	            "telefonoMovil": usuario.telefonoMovil,
+	            "direccion": usuario.direccion,
+	            "tipoUsuarioId": usuario.tipoUsuarioId,
+				"pedidosRechazados":pedidosRechazados,
+				"pedidosActivos":pedidosActivos,
+				"pedidosAceptados":pedidosAceptados
+			});
+		}else{
+			if(cuentas.length==0){
+				return {tipoMensaje:2,descripcion:'No existe una cuenta de usuario con estos datos.'};
 			}else{
-				if(res.length==0){
-					return {tipoMensaje:2,descripcion:'No existe una cuenta de usuario con estos datos.'};
-				}else{
-					return {tipoMensaje:3,descripcion:'ERROR RARO, existen mas de una cuenta de usuario vinculado con estos datos'};				
-				}
+				return {tipoMensaje:3,descripcion:'ERROR RARO, existen mas de una cuenta de usuario vinculado con estos datos'};				
 			}
-		});
+		}
+		return resultado;
 	}
 }
