32836cb56b675af27bf7a0ce2b478116894e9391
giordano200699 <giordano200699@gmail.com>
2019-06-14 13:35:15 -0500

    Mejorando relacion Temas-Temas

--- src/esquemas/tema.schema.ts
+++ src/esquemas/tema.schema.ts
@@ -4,9 +4,5 @@ export const TemaSchema = new Schema({
     temaId: String,
     nombre: String,
     descripcion: String,
-    temaR1: String,
-    temaR2: String,
-    temaR3: String,
-    temaR4: String,
-    temaR5: String
+    temas: [{temaId:String, peso:Number}]
 });
--- src/interfaces/Tema.ts
+++ src/interfaces/Tema.ts
@@ -5,9 +5,5 @@ export interface Tema extends Document {
     temaId?: String,
     nombre: String,
     descripcion?: String,
-    temaR1?: String,
-    temaR2?: String,
-    temaR3?: String,
-    temaR4?: String,
-    temaR5?: String
+    temas?: [{temaId:String, peso:Number}]
 }
